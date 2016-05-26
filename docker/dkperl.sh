ea22098b8581f91b46ba4aa18b213833e6bccbac
Mike Cramer <imanerd@me.com>
2016-05-26 10:38:22 -0500

    Moved docker alias

--- docker/dkperl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-
-docker run -it --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp perl:5.20 bash
--- profiles/public/alias.sh
+++ profiles/public/alias.sh
@@ -14,7 +14,6 @@ alias lsaf="clear; lsa; ls -la *"
 # alias twopen="open -a /Applications/BBedit.app/ "
 alias qlynx="cd ~/Downloads/; lynx -cookie_file=/tmp/lynxcookie -accept_all_cookies; rm /tmp/lynxcookie"
 alias primedocker="/Applications/Docker/Docker\ Quickstart\ Terminal.app/Contents/Resources/Scripts/start.sh"
-alias dkperl='docker run -it --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp perl:5.20 perl'
 
 # Quick way to rebuild the Launch Services database and get rid of duplicates in the Open With submenu.
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
