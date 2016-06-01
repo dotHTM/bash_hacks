6724f857d5613f2a308d7e636345a6753144b992
Mike Cramer <imanerd@me.com>
2016-06-01 09:48:40 -0500

    more echo

--- docker/dkFullReset.sh
+++ docker/dkFullReset.sh
@@ -6,14 +6,14 @@
 echo "=== Docker Full Reset ==="
 echo "-- ps --"
 psList=`docker ps --all --format="{{.ID }}" | uniq`
+echo "-- removing ps --"
 for p in $psList; do
 	echo $p
 	docker rm -f $p;
 done
-
 echo "-- images --" 
-
 imageList=`docker images --format="{{.ID }}" | uniq`
+echo "-- removing images --"
 for i in $imageList; do
 	echo $i
 	docker rmi -f $i
