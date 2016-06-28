6db211c04b155ff1eae6e3ca91db5a0671c9bdc7
Mike Cramer <imanerd@me.com>
2016-06-28 18:13:20 -0500

    Fancy Prompt, Export to swap editor to Subl

--- profiles/public/prompt.sh
+++ profiles/public/prompt.sh
@@ -2,4 +2,12 @@
 #
 # 
 
-export PS1="[ \u @ \h : \W ]"
+tput init
+bold="\[`tput bold`\]"
+undl="\[`tput smul`\]"
+revE="\[`tput rev`\]"
+blnk="\[`tput blink`\]"
+nrml="\[`tput sgr0`\]"
+
+export PS1="[ ${bold}\u${nrml} @ ${undl}${bold}\h${nrml} : ${bold}\W${nrml} ]"
+
