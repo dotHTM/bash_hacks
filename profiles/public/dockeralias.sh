93f48f1574d9ba098b97b3ba4e2a932717746994
Mike Cramer <imanerd@me.com>
2016-08-17 13:21:27 -0500

    docker

--- profiles/public/dockeralias.sh
+++ profiles/public/dockeralias.sh
@@ -3,3 +3,6 @@
 # 
 
 alias dkperl="docker run -it --rm --name my-running-perl-script -v \"$PWD\":/usr/src/myapp -w /usr/src/myapp perl:5.20 perl"
+alias dcu="docker-compose up"
+alias dcb="docker-compose build; docker-compose up"
+alias dcrsubl="docker run -ti --rm testbuild bash"
