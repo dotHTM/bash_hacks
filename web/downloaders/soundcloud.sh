ab5441e581935a1e0be1460f96c6d7488c46ea97
Mike Cramer <imanerd@me.com>
2016-09-15 10:44:15 -0500

    downloader

--- web/downloaders/soundcloud.sh
+++ web/downloaders/soundcloud.sh
@@ -4,8 +4,12 @@
 
 # Get this info from your browser. Load up a track and check the inspectors for the API call to grab the streams. Get from API URL call. I'm not sure if these are session specific, perhaps related to login session, so consider NOT sharing. Plus likely to be invalidated durring normal SoundCloud development.
 
-appVersion=""
-clientID=""
+# Ex: 
+# From the XHRs in Safari's Resource inspector, the "streams" API call uses the URL: 
+#		https://api.soundcloud.com/i1/tracks/146805680/streams?client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&app_version=1471599913
+
+app_version="1471599913"
+client_id="02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea"
 
 main()
 {
@@ -40,7 +44,7 @@ getAnTrack()
 	
 	# TODO: if trackID is empty, report error, exit
 	
-	trackURL=`curl -s "https://api.soundcloud.com/i1/tracks/${trackID}/streams?client_id=${clientID}&app_version=${appVersion}" | perl -pe 's|.*(https://cf-media.sndcdn.com.*?)".*|$1|gi; s|\\u0026|&|gi'`
+	trackURL=`curl -s "https://api.soundcloud.com/i1/tracks/${trackID}/streams?client_id=${client_id}&app_version=${app_version}" | perl -pe 's|.*(https://cf-media.sndcdn.com.*?)".*|$1|gi; s|\\u0026|&|gi'`
 	
 	# TODO: if trackURL is empty, report error, exit
 	
