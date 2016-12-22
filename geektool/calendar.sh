b522163d9e88e7d682bc7a627cb2947d5b4ecd9f
Michael Cramer <imanerd@me.com>
2016-12-21 19:04:55 -0600

    Geektool

---
+++ geektool/calendar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# calendar.sh
+
+echo "<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello world</title>
+</head>
+<body>
+<p>Some text</p>
+</body>
+</html>"
---
+++ geektool/time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# time.sh
+#   
+
+
+
+
+date "+%A%n%Y-%m-%d%n  %H:%M:%S%n %z"
+date -u "+  %H:%M:%S%n %z%n%s"
