01345ca11cbf51163fc4cfb905c9b396ab19fc6a
Michael Cramer <imanerd@me.com>
2017-03-22 16:18:34 -0500

    Man, I wasn’t expecting to see that Perl script slip itself into this project, but hey, easier to control individual colors.

---
+++ geektool/calendar.pl
@@ -0,0 +1,84 @@
+#!/usr/local/bin/perl
+# calendar.pl
+#   Description
+#
+#   Written by Mike Cramer
+#   Started on 2017-03-22
+
+use strict;
+use warnings;
+
+use HTML::Template;
+
+# open the html template
+my $template = HTML::Template->new( filename => '/Users/cramerm/Projects/github/bash_hacks/geektool/calendar.tmpl' );
+
+
+
+my $systemUptimeResult = `uptime`;
+
+my $TIME = $systemUptimeResult =~ /^.*?up (.*?), \d+ user/ ? $1 : '';
+my $USERCOUNT = $systemUptimeResult =~ /\b(\d+ user\w), / ? $1 : '';
+my $LOAD_AVERAGES = $systemUptimeResult =~ /load averages: (.*)/ ? $1 : '';
+
+my $CALENDAR = `cal` ;
+chop $CALENDAR;
+
+my $todaysDate            = `date "+%d"`;
+chop $todaysDate;
+
+my $headDate = $CALENDAR =~ /(.*)\nSu/ ? $1 : '';
+my $dayList = $CALENDAR =~ /(Su.*Sa)/ ? $1 : '';
+my $leftDays = $CALENDAR =~ /Sa((.*\n?)+\ )$todaysDate\ / ? $1 : '';
+my $rightDays = $CALENDAR =~ /\ $todaysDate(\ (.*\n?)+)/ ? $1 : '';
+my $userList = `who`;
+
+my $DAY             = `date "+%A"`;
+chop $DAY;
+my $DATE            = `date "+%Y-%m-%d"`;
+chop $DATE;
+my $JULIAN          = `date "+%j"`;
+chop $JULIAN;
+my $LOCAL_TIME      = `date "+%H:%M:%S"`;
+chop $LOCAL_TIME;
+my $LOCAL_TIME_ZONE = `date "+%z"`;
+chop $LOCAL_TIME_ZONE;
+my $UTC_TIME        = `date -u "+%H:%M:%S"`;
+chop $UTC_TIME;
+my $UTC_TIME_ZONE   = `date -u "+%z"`;
+chop $UTC_TIME_ZONE;
+my $UNIX_TIME       = `date "+%s"`;
+chop $UNIX_TIME;
+
+my $lastReboot = `who -b`;
+chop $lastReboot;
+
+
+$template->param( RAW             => $systemUptimeResult );
+$template->param( CALENDARHEAD    => $headDate );
+$template->param( CALENDARLIST    => $dayList );
+$template->param( CALENDARLEFT    => $leftDays );
+$template->param( CALENDARTODAY   => $todaysDate );
+$template->param( CALENDARRIGHT   => $rightDays );
+$template->param( TIME            => $TIME               );
+$template->param( USERCOUNT       => $USERCOUNT          );
+$template->param( USERLIST        => $userList           );
+$template->param( LOAD_AVERAGES   => $LOAD_AVERAGES      );
+$template->param( DAY             => $DAY                );
+$template->param( DATE            => $DATE               );
+$template->param( JULIAN          => $JULIAN             );
+$template->param( LOCAL_TIME      => $LOCAL_TIME         );
+$template->param( LOCAL_TIME_ZONE => $LOCAL_TIME_ZONE    );
+$template->param( UTC_TIME        => $UTC_TIME           );
+$template->param( UTC_TIME_ZONE   => $UTC_TIME_ZONE      );
+$template->param( UNIX_TIME       => $UNIX_TIME          );
+
+# fill in some parameters
+# $template->param( HOME => $ENV{HOME} );
+# $template->param( PATH => $ENV{PATH} );
+
+### send the obligatory Content-Type and print the template output
+# print "Content-Type: text/html\n\n"; ## for some reason, Geektool renders this line litterally.
+print $template->output;
+
+
--- geektool/time.sh
+++ geektool/time.sh
@@ -3,10 +3,70 @@
 #   
 
 
+colorEnabled=1
+
+# systemUptime=`uptime`
 
 
-date    "+%A%n%Y-%m-%d %j"
-date    "+ L %H:%M:%S %z"
-date -u "+ U %H:%M:%S %z"
-date    "+ %s"
+tputColors(){
+	
+	tput_nrml=`tput sgr0`
+	tput_dim=`tput dim`
+	tput_bold=`tput bold`
+	tput_undl=`tput smul`
+	tput_revE=`tput rev`
+	tput_blnk=`tput blink`
+	
+	tput_black=`tput setaf 0`
+	tput_red=`tput setaf 1`
+	tput_green=`tput setaf 2`
+	tput_yellow=`tput setaf 3`
+	tput_blue=`tput setaf 4`
+	tput_magenta=`tput setaf 5`
+	tput_cyan=`tput setaf 6`
+	tput_white=`tput setaf 7`
+	
+}
 
+
+loadStyles(){
+	themeDefault=$tput_white
+	
+	themeUptime=$tput_cyan
+	themeUsers=$tput_white
+	themeLoad=$tput_cyan
+	themeLoadNumbers=$tput_cyan$tput_test
+	
+	themeDay=$tput_red
+	themeDate=$themeDefault
+	themeJulian=$tput_yellow
+	
+	themeLocalTime=$tput_green
+	themeLocalTimeZone=$tput_cyan
+	
+	themeUtcTime=$tput_green
+	themeUtcTimeZone=$tput_cyan
+	
+	themeUnixTime=$tput_white
+}
+
+main () {
+
+	if (( colorEnabled )); then
+		tputColors
+		loadStyles
+	fi
+
+	echo -n ${themeDefault}
+	echo -n ${themeUptime}
+	echo `uptime` | perl -pe "s/, /${themeDefault}\n /g" | perl -pe "s/: /:${themeDefault}\n  ${themeLoadNumbers}/g"
+	echo -n ${themeDefault}
+
+	date    "+${themeDay}%A${themeDefault}%n${themeDate}%Y-%m-%d${themeDefault} ${themeJulian}%j${themeDefault}"
+	date    "+ L ${themeLocalTime}%H:%M:%S${themeDefault} ${themeLocalTimeZone}%z${themeDefault}"
+	date -u "+ U ${themeUtcTime}%H:%M:%S${themeDefault} ${themeUtcTimeZone}%z${themeDefault}"
+	date    "+ ${themeUnixTime}%s${themeDefault}"
+}
+
+
+main
