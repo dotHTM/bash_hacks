f46dd02353c775b473d577cffb51a18f0117381a
Michael Cramer <imanerd@me.com>
2017-09-11 16:08:52 -0500

    International Timezone display

---
+++ amuse/internationalTimes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+zones='America/Los_Angeles
+America/Chicago
+America/New_York
+UTC
+Australia/Sydney
+'
+
+format="+  %m/%d - %H:%M %Z"
+
+line="="
+
+for mytz in $zones; do
+    echo $mytz
+    echo $mytz | perl -pe "s|.|$line|gi" 
+    TZ=$mytz date "$format"
+    echo
+done
