7ece8072b99698f3e0f3aaec1e03bc94ef19267c
Mike Cramer <imanerd@me.com>
2017-10-28 02:25:28 -0500

    Get perlcritic and syntax checking. Run as a build system in Sublime Text!

--- dev/perlProcess.sh
+++ dev/perlProcess.sh
@@ -5,28 +5,36 @@ someFile=$1 && shift
 
 
 if [[ -n $someFile ]]; then
-    someOutputFilePath=${someFile}.md
+    someOutputFilePath=${someFile}_critic.md
 
     criticSeverity=1
-    criticBrevityFormat='<<%f>> at <%l:%c> ::  %m, %e (%s)\\n'
-    criticVerboseFormat='## <%l:%c> :: %m, %e (%s)\\n\\n```perl\\n%r\\n```\\n\\n%d\\n\\n\\n\\n'
+    criticBrevityFormat=' %f at <%l:%c> ::  %m, %e (%s)\\n'
+    criticVerboseFormat='## %m <%l:%c> (%s)\\n\\n```perl\\n%r\\n```\\n\\n%d\\n\\n**Policy modules**\\n```perl\\n%p\\n```\\n> %e\\n\\n'
 
     perltidy -b $someFile
-    perlcritic -$criticSeverity --verbose "$criticBrevityFormat" $someFile
+    perlcritic -$criticSeverity --verbose 4 $someFile
     perlcritic -$criticSeverity --verbose "$criticVerboseFormat" $someFile \
-        | perl -pe "s/^(    )/>\1/gi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
-        | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
+        | perl -pe "s/^(    )//gi" \
+        | perl -pe "s/^  (\w.*;)/    \1/gi" \
+        | perl -pe "s/\\\`(.*?)(\S)'/\\\`\1\2\\\`/gmi" \
         > $someOutputFilePath
 
-    subl $someOutputFilePath
+
+
+    # subl $someOutputFilePath
+    open -a "/Applications/Marked 2.app" $someOutputFilePath
+
+    perl -c $someFile
 
 fi
 
+
+
+        # | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
+        # | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
+        # | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
+        # | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
+        # | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
+        # | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
+        # | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
+        # | perl -pe "s/^> (.*?)\\\`(.*?)'/> \1\\\`(.*?)\\\`/gmi" \
