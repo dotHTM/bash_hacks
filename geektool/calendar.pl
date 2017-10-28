f231677ebddfa2af6d132bfe34631c2c79ce21a2
Mike Cramer <imanerd@me.com>
2017-10-28 02:24:20 -0500

    Cleanup, refactoring, and enhancements suggested by perlcritic

--- geektool/calendar.pl
+++ geektool/calendar.pl
@@ -5,128 +5,125 @@
 #   Written by Mike Cramer
 #   Started on 2017-03-22
 
-use strict ;
-use warnings ;
+use feature ':5.16';
+use strict;
+use warnings;
 
-use Getopt::Std ;
-my %opts ;
-getopts ( 'hdvno', \%opts ) ;
+use IPC::Open3 'open3';
 
-our $help_mode  = $opts{ 'h' } ;
-our $debug_mode = $opts{ 'd' } ;
+use autodie qw(open close);
 
-our $dryRun_mode = $opts{ 'n' } ;
-our $output_mode = $opts{ 'o' } ;
+use version; our $VERSION = qv(0.0001);
 
-if ( $help_mode ) {
-    print "Usage: $0 -hdno\n" ;
-    print join (
-        "\n\t",
-        (
-            "Options:",
-            "-h\thelp_mode",
-            "-d\tget debug messages",
-            "-n\tdon't print to screen",
-            "-o\toutupt to file"
-        )
-    ) ;
+use English qw( -no_match_vars );
 
-    print "\n" ;
+use Readonly;
+Readonly my $EMPTY = qw{};
 
-    exit 0 ;
+use Getopt::Std;
+my %opts;
+getopts( 'hdvno', \%opts );
+
+my $help_mode  = $opts{'h'};
+my $debug_mode = $opts{'d'};
+
+my $dry_run_mode = $opts{'n'};
+my $output_mode  = $opts{'o'};
+
+if ($help_mode) {
+    say "Usage: $PROGRAM_NAME -hdno";
+    say join
+      "\n\t",
+      (
+        'Options:',
+        "-h\thelp_mode",
+        "-d\tget debug messages",
+        "-n\tdon't print to screen",
+        "-o\toutupt to file"
+      );
+
+    exit 0;
 }
 
-open OUTPUTFILE, ">./cal.html" ;
-
-use HTML::Template ;
+use HTML::Template;
 
 # open the html template
-my $template =
-    HTML::Template->new (
-    filename => 'calendar.tmpl' ) ;
+my $template = HTML::Template->new( filename => 'calendar.tmpl' );
 
-sub debugMessage($$) {
-    my ( $key, $value ) = @_ ;
-    my $breakLine = "" ;
-    $breakLine = "\n" if ( $value =~ /\n/ ) ;
-    print "$key => $breakLine'$value'\n\n" if $debug_mode ;
+sub debug_message {
+    my ( $key, $value ) = @_;
+    my $break_line = $EMPTY;
+    if ( $value =~ /\n/xms ) { $break_line = "\n"; }
+    if ($debug_mode) { say "$key => $break_line'$value'" }
+    return;
 }
 
-sub passTemplateKV($ $) {
-    my ( $key, $value ) = @_ ;
-    debugMessage $key, $value ;
-    $template->param ( $key => $value ) ;
+my $system_uptime_result = `uptime`;
+
+debug_message( 'systemUptimeResult', $system_uptime_result );
+
+my $time = $system_uptime_result =~ /^.*?up (.*?), \d+ user/xms ? $1 : $EMPTY;
+my $usercount = $system_uptime_result =~ /(\d+ users?), /xms ? $1 : $EMPTY;
+my $load_averages =
+  $system_uptime_result =~ /load averages: (.*)/xms ? $1 : $EMPTY;
+
+my $calendar = `cal -h`;
+chop $calendar;
+
+use Time::Piece;
+
+my $tp_localtime = localtime;
+my $tp_gmtime    = gmtime;
+
+my $todays_date = $tp_localtime->mday;
+
+my $head_date  = $calendar =~ /(.*)\nSu/xms                       ? $1 : $EMPTY;
+my $day_list   = $calendar =~ /(Su.*Sa)/xms                       ? $1 : $EMPTY;
+my $left_days  = $calendar =~ /Sa((.*\n?)+ ?)\b$todays_date\b/xms ? $1 : $EMPTY;
+my $right_days = $calendar =~ /\b$todays_date\b( (.*\n?)+)/xms    ? $1 : $EMPTY;
+my $user_list  = `who`;
+
+my $last_reboot = `who -b`;
+chop $last_reboot;
+
+my %data_hash = (
+    'RAW'             => $system_uptime_result,
+    'CALENDARHEAD'    => $head_date,
+    'CALENDARLIST'    => $day_list,
+    'CALENDARLEFT'    => $left_days,
+    'CALENDARTODAY'   => $tp_localtime->mday,
+    'CALENDARRIGHT'   => $right_days,
+    'TIME'            => $time,
+    'USERCOUNT'       => $usercount,
+    'USERLIST'        => $user_list,
+    'LOAD_AVERAGES'   => $load_averages,
+    'DAY'             => $tp_localtime->fullday,
+    'DATE'            => $tp_localtime->date,
+    'JULIAN'          => $tp_localtime->yday,
+    'LOCAL_TIME'      => $tp_localtime->time,
+    'LOCAL_TIME_ZONE' => $tp_localtime->tzoffset,
+    'UTC_TIME'        => $tp_gmtime->time,
+    'UTC_DATE'        => $tp_gmtime->date,
+    'UTC_TIME_ZONE'   => $tp_gmtime->tzoffset,
+    'UNIX_TIME'       => $tp_gmtime->epoch,
+);
+
+foreach my $key ( keys %data_hash ) {
+    my $value = $data_hash{$key};
+    debug_message( $key, $value );
+    $template->param( $key => $value );
 }
-
-my $systemUptimeResult = `uptime` ;
-
-debugMessage "systemUptimeResult", $systemUptimeResult ;
-
-my $TIME          = $systemUptimeResult =~ /^.*?up (.*?), \d+ user/ ? $1 : '' ;
-my $USERCOUNT     = $systemUptimeResult =~ /(\d+ users?), /         ? $1 : '' ;
-my $LOAD_AVERAGES = $systemUptimeResult =~ /load averages: (.*)/    ? $1 : '' ;
-
-my $CALENDAR = `cal -h` ;
-chop $CALENDAR ;
-
-my $todaysDate = `date "+%e"` ;
-$todaysDate = $todaysDate =~ /(\d+)/ ? $1 : '' ;
-
-my $headDate  = $CALENDAR =~ /(.*)\nSu/                    ? $1 : '' ;
-my $dayList   = $CALENDAR =~ /(Su.*Sa)/                    ? $1 : '' ;
-my $leftDays  = $CALENDAR =~ /Sa((.*\n?)+\ ?)\b$todaysDate\b/ ? $1 : '' ;
-my $rightDays = $CALENDAR =~ /\b$todaysDate\b(\ (.*\n?)+)/   ? $1 : '' ;
-my $userList  = `who` ;
-
-my $DAY = `date "+%A"` ;
-chop $DAY ;
-my $DATE = `date "+%Y-%m-%d"` ;
-chop $DATE ;
-my $JULIAN = `date "+%j"` ;
-chop $JULIAN ;
-my $LOCAL_TIME = `date "+%H:%M:%S"` ;
-chop $LOCAL_TIME ;
-my $LOCAL_TIME_ZONE = `date "+%z"` ;
-chop $LOCAL_TIME_ZONE ;
-my $UTC_TIME = `date -u "+%H:%M:%S"` ;
-chop $UTC_TIME ;
-my $UTC_DATE = `date -u "+%m-%d"` ;
-chop $UTC_DATE ;
-my $UTC_TIME_ZONE = `date -u "+%z"` ;
-chop $UTC_TIME_ZONE ;
-my $UNIX_TIME = `date "+%s"` ;
-chop $UNIX_TIME ;
-
-my $lastReboot = `who -b` ;
-chop $lastReboot ;
-
-passTemplateKV ( "RAW",             $systemUptimeResult ) ;
-passTemplateKV ( "CALENDARHEAD",    $headDate ) ;
-passTemplateKV ( "CALENDARLIST",    $dayList ) ;
-passTemplateKV ( "CALENDARLEFT",    $leftDays ) ;
-passTemplateKV ( "CALENDARTODAY",   $todaysDate ) ;
-passTemplateKV ( "CALENDARRIGHT",   $rightDays ) ;
-passTemplateKV ( "TIME",            $TIME ) ;
-passTemplateKV ( "USERCOUNT",       $USERCOUNT ) ;
-passTemplateKV ( "USERLIST",        $userList ) ;
-passTemplateKV ( "LOAD_AVERAGES",   $LOAD_AVERAGES ) ;
-passTemplateKV ( "DAY",             $DAY ) ;
-passTemplateKV ( "DATE",            $DATE ) ;
-passTemplateKV ( "JULIAN",          $JULIAN ) ;
-passTemplateKV ( "LOCAL_TIME",      $LOCAL_TIME ) ;
-passTemplateKV ( "LOCAL_TIME_ZONE", $LOCAL_TIME_ZONE ) ;
-passTemplateKV ( "UTC_TIME",        $UTC_TIME ) ;
-passTemplateKV ( "UTC_DATE",        $UTC_DATE ) ;
-passTemplateKV ( "UTC_TIME_ZONE",   $UTC_TIME_ZONE ) ;
-passTemplateKV ( "UNIX_TIME",       $UNIX_TIME ) ;
 
 # fill in some parameters
-# $template->param( HOME => $ENV{HOME} );
-# $template->param( PATH => $ENV{PATH} );
+# $template->param( HOME => $env{HOME} );
+# $template->param( PATH => $env{PATH} );
 
 ### send the obligatory Content-Type and print the template output
 # print "Content-Type: text/html\n\n"; ## for some reason, Geektool renders this line litterally.
-print $template->output unless $dryRun_mode ;
+if ( !$dry_run_mode ) { print $template->output; }
 
-print OUTPUTFILE $template->output if $output_mode ;
-
+if ($output_mode) {
+    open my $calendar_outfile, '>', './cal.html';
+    print {$calendar_outfile} $template->output;
+    close $calendar_outfile;
+}
