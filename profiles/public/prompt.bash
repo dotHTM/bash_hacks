4939642f2988588b837ad4861a28fdf2b8bb258e
Michael Cramer <imanerd@me.com>
2018-02-08 15:56:11 -0600

    I made a mess of colors...

--- profiles/public/prompt.bash
+++ profiles/public/prompt.bash
@@ -10,9 +10,16 @@ escape_wrap(){
     echo "\[$1\]"
 }
 
+##### THIS IS A HECKIN MESS!!!!
+
 ## Color shortcuts
 if [[ -n $PS1 ]]; then
     tput init
+    
+    theme_1=196
+    theme_2=46
+    theme_3=208
+    theme_4=226
     
     ## Raw values
     tput_bold=`tput bold`
@@ -20,10 +27,10 @@ if [[ -n $PS1 ]]; then
     tput_revE=`tput rev`
     tput_blnk=`tput blink`
     tput_nrml=`tput sgr0`
-    tput_theme1color=`tput setaf 1`
-    tput_theme2color=`tput setaf 2`
-    tput_theme3color=`tput setaf 3`
-    tput_theme4color=`tput setaf 6`
+    tput_theme1color=`tput setaf ${theme_1}`
+    tput_theme2color=`tput setaf ${theme_2}`
+    tput_theme3color=`tput setaf ${theme_3}`
+    tput_theme4color=`tput setaf ${theme_4}`
 
     ## Wrapped values for PS1
     wrapped_tput_bold=`escape_wrap $tput_bold`
@@ -36,6 +43,31 @@ if [[ -n $PS1 ]]; then
     wrapped_tput_theme3color=`escape_wrap $tput_theme3color`
     wrapped_tput_theme4color=`escape_wrap $tput_theme4color`
     
+    bracket_color="${tput_theme1color}${tput_bold}"
+    user_color="${tput_theme3color}"
+    hostname_color="${tput_theme4color}${tput_undl}"
+    prompt_color="${tput_theme2color}${tput_bold}"
+    reset_color="${tput_nrml}"
+    
+    wr_bracket_color=`escape_wrap ${bracket_color}`
+    wr_user_color=`escape_wrap ${user_color}`
+    wr_hostname_color=`escape_wrap ${hostname_color}`
+    wr_prompt_color=`escape_wrap ${prompt_color}`
+    wr_reset_color=`escape_wrap ${reset_color}`
+    
+    theme_info(){
+        echo " ${tput_theme1color}tput_theme1color${tput_nrml} = ${theme_1}"
+        echo " ${tput_theme2color}tput_theme2color${tput_nrml} = ${theme_2}"
+        echo " ${tput_theme3color}tput_theme3color${tput_nrml} = ${theme_3}"
+        echo " ${tput_theme4color}tput_theme4color${tput_nrml} = ${theme_4}"
+        echo ""
+        echo " ${bracket_color}bracket_color ${tput_nrml} = ${theme_1}"
+        echo " ${user_color}user_color    ${tput_nrml} = ${theme_2}"
+        echo " ${hostname_color}hostname_color${tput_nrml} = ${theme_3}"
+        echo " ${prompt_color}prompt_color  ${tput_nrml} = ${theme_4}"
+    }
+    
+    
     ## HR
     term_width=`tput cols`
     for (( i = 0; i < $term_width; i++ )); do
@@ -43,12 +75,13 @@ if [[ -n $PS1 ]]; then
     done
     
     ## Display a message the terminal is Interactive and if Screen
-    echo -n "  ${tput_theme1color}>>${tput_nrml} ${tput_theme2color}Interactive Shell${tput_nrml} : ${tput_theme4color}Lvl ${SHLVL}${tput_nrml} ${tput_theme1color}<<${tput_nrml}  "
+    echo -n "  ${bracket_color}>>${tput_nrml} ${user_color}Interactive Shell${tput_nrml} : ${hostname_color}Lvl ${SHLVL}${tput_nrml} ${bracket_color}<<${tput_nrml}  "
     if [[ "$TERM" == "screen" ]]; then
-        echo -n "  ${tput_theme1color}>>${tput_nrml} ${tput_theme3color}Screen${tput_nrml} ${tput_theme1color}<<${tput_nrml}  "
+        echo -n "  ${bracket_color}>>${tput_nrml} ${prompt_color}Screen${tput_nrml} ${bracket_color}<<${tput_nrml}  "
     fi
     echo
 fi ## /Color shortcuts
+
 
 # export PS1="[ ${bold}\u${nrml} @ ${undl}${bold}\h${nrml} : ${bold}\W${nrml} ]"
 
@@ -73,10 +106,10 @@ dullPrompt(){
 }
 
 boldPrompt(){
-    change_PS1 "${wrapped_tput_theme1color}${wrapped_tput_bold}[${wrapped_tput_nrml} ${wrapped_tput_theme3color}\u${wrapped_tput_nrml} ${wrapped_tput_theme1color}${wrapped_tput_bold}@${wrapped_tput_nrml} ${wrapped_tput_theme4color}${wrapped_tput_undl}${bashHostNameReplacement}${wrapped_tput_nrml} ${wrapped_tput_theme1color}${wrapped_tput_bold}:${wrapped_tput_nrml} \W ${wrapped_tput_theme1color}${wrapped_tput_bold}]${wrapped_tput_nrml}${commandLineDeliminator}${wrapped_tput_theme2color}${wrapped_tput_bold}\! >${wrapped_tput_nrml} "
+    change_PS1 "${wr_bracket_color}[${wr_reset_color} ${wr_user_color}\u${wr_reset_color} ${wr_bracket_color}@${wr_reset_color} ${wr_hostname_color}${bashHostNameReplacement}${wr_reset_color} ${wr_bracket_color}:${wr_reset_color} \W ${wr_bracket_color}]${wr_reset_color}${commandLineDeliminator}${wr_prompt_color}\! >${wr_reset_color} "
 }
 
 shortPrompt(){
-    change_PS1 "${wrapped_tput_theme1color}${wrapped_tput_bold}[${wrapped_tput_nrml} ${wrapped_tput_theme1color}${wrapped_tput_bold}:${wrapped_tput_nrml} \W ${wrapped_tput_theme1color}${wrapped_tput_bold}]${wrapped_tput_nrml}${commandLineDeliminator}${wrapped_tput_theme2color}${wrapped_tput_bold}\! >${wrapped_tput_nrml} "
+    change_PS1 "${wr_bracket_color}[${wr_reset_color} ${wr_bracket_color}:${wr_reset_color} \W ${wr_bracket_color}]${wr_reset_color}${commandLineDeliminator}${wr_prompt_color}\! >${wr_reset_color} "
 }
 
