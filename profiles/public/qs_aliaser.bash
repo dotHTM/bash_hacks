46e63f13cb6fa49153675b25d91a241498c5adad
Michael Cramer <imanerd@me.com>
2018-03-12 13:02:43 -0500

    Dupe logic

--- profiles/public/qs_aliaser.bash
+++ profiles/public/qs_aliaser.bash
@@ -1,7 +1,5 @@
 # qs_aliaser.bash
 
-if [[ -e $BH_PRIVATE_BASHRC_PATH ]]; then
-
 BH_CONNECTION_LIST=`cat $BH_PRIVATE_BASHRC_PATH/quicksshfs_alias.cfg`
 
 for someServer in $BH_CONNECTION_LIST; do
@@ -23,6 +21,4 @@ for someServer in $BH_CONNECTION_LIST; do
   alias qs_${serverShortcutName}_f="quicksshfs.sh -r ${someServer} -v ${volPath} -f"
   alias qs_${serverShortcutName}_l="cd \`quicksshfs.sh -r ${someServer} -v ${volPath} -l\`"
 done
-
-fi
 
