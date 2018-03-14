2305b61dfc31fbd563cf102e26d64b5e6cca66a8
Michael Cramer <imanerd@me.com>
2018-03-14 15:22:28 -0500

    sample config script

---
+++ web/tapi/policies/policy_config.bash
@@ -0,0 +1,9 @@
+####
+
+export MAIN_ACCOUNT="dothtm"
+export SECONDARY_ACCOUNT="kiodane"
+
+export MAIN_TO_SECONDARY_TRANSFER_LIST="private-list-to-transfer"
+export SECONDARY_TO_MAIN_TRANSFER_LIST="private-list-to-transfer"
+
+
