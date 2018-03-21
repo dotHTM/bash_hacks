2ba1cf4d8242d34572ff48ebb248821f8eebc298
Michael Cramer <imanerd@me.com>
2018-03-21 17:28:53 -0500

    that dedupe is broken

--- profiles/bashrc.bash
+++ profiles/bashrc.bash
@@ -14,34 +14,13 @@ path_add_element(){
 
 if [[ -n $PROFILE_DIR ]]; then
     for anFile in `find $PROFILE_DIR/public -iname "*.bash"`; do
-    # echo $anFile
-    source $anFile
-done
+        source $anFile
+    done
 
-if [[ -n $BH_PRIVATE_BASHRC_PATH ]]; then
-    for anFile in `find $BH_PRIVATE_BASHRC_PATH -iname "*.bash"`; do
-    # echo $anFile
-    source $anFile
-done
-fi
-
-dedupe_paths(){
-    ## Dedupe paths taken from https://unix.stackexchange.com/questions/40749/remove-duplicate-path-entries-with-awk-command
-    if [ -n "$PATH" ]; then
-        old_PATH=$PATH:; PATH=
-        while [ -n "$old_PATH" ]; do
-            x=${old_PATH%%:*}       # the first remaining entry
-            case $PATH: in
-                *:"$x":*) ;;          # already there
-                *) PATH=$PATH:$x;;    # not there yet
-                #
-            esac
-            old_PATH=${old_PATH#*:}
+    if [[ -n $BH_PRIVATE_BASHRC_PATH ]]; then
+        for anFile in `find $BH_PRIVATE_BASHRC_PATH -iname "*.bash"`; do
+            source $anFile
         done
-        PATH=${PATH#:}
-        unset old_PATH x
     fi
-}
-
 fi
 
