b777d505956133392678b1c43b0a722699c2a8af
Mike Cramer <imanerd@me.com>
2018-08-27 17:01:11 -0500

    All the cool kids and their ssh keys

---
+++ util/transfer-ssh-key.sh
@@ -0,0 +1,27 @@
+#!/usr/local/bin/bash
+# transfer-ssh-key.sh
+
+
+if [[ $BASH_VERSINFO < 4 ]]; then
+    echo -n "bash version is \"$BASH_VERSINFO\" and I require at least 4"
+    exit
+fi
+
+host_list=$@
+public_key=`cat $HOME/.ssh/id_rsa.pub`
+
+for host in $host_list; do
+    ssh "${host}" -t \
+        "echo '$public_key' >> .ssh/authorized_keys;
+        cp .ssh/authorized_keys .ssh/tmp; cat .ssh/tmp | sort | uniq > .ssh/authorized_keys;
+        echo '$public_key' >> .ssh/authorized_keys2;
+        cp .ssh/authorized_keys2 .ssh/tmp; cat .ssh/tmp | sort | uniq > .ssh/authorized_keys2;
+        vim .ssh/authorized_keys;
+        vim .ssh/authorized_keys2;
+        "
+    
+done
+
+
+
+
