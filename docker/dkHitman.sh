7de9b9eb59912b95f100685f0f13868a9ee005d2
Mike Cramer <imanerd@me.com>
2018-12-04 17:35:25 -0600

    check for binary

--- docker/dkHitman.sh
+++ docker/dkHitman.sh
@@ -1,19 +1,19 @@
 #!/usr/local/bin/bash
 # dkFullReset.sh
 
-echo "== Containers =="
-for i in $(docker ps --all -q); do
-	docker rm -f $i
-done
-
-echo "== Images =="
-for i in $(docker images --all -q); do
-	docker rmi -f $i
-done
-
-echo "== Volumes =="
-for i in $(docker volume ls -q); do
-	docker volume rm $i
-done
-
-
+if [[ -n `which docker` ]]; then
+    echo "== Containers =="
+    for i in $(docker ps --all -q); do
+        docker rm -f $i
+    done
+    echo "== Images =="
+    for i in $(docker images --all -q); do
+        docker rmi -f $i
+    done
+    echo "== Volumes =="
+    for i in $(docker volume ls -q); do
+        docker volume rm $i
+    done
+else
+    echo "Docker not installed"
+fi
--- docker/dkJanitor.sh
+++ docker/dkJanitor.sh
@@ -1,19 +1,21 @@
 #!/usr/local/bin/bash
 # dkJanitor.sh
 
-echo "== Containers =="
-for i in $(docker ps -q -f 'status=exited'); do
-	docker rm $i
-done
+if [[ -n `which docker` ]]; then
+    echo "== Containers =="
+    for i in $(docker ps -q -f 'status=exited'); do
+        docker rm $i
+    done
 
-echo "== Images =="
-for i in $(docker images -q -f "dangling=true"); do
-	docker rmi $i
-done
+    echo "== Images =="
+    for i in $(docker images -q -f "dangling=true"); do
+        docker rmi $i
+    done
 
-echo "== Volumes =="
-for i in $(docker volume ls -q -f 'dangling=true'); do
-	docker volume rm $i
-done
-
-
+    echo "== Volumes =="
+    for i in $(docker volume ls -q -f 'dangling=true'); do
+        docker volume rm $i
+    done
+else
+    echo "Docker not installed"
+fi
