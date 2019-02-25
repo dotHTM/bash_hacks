bd1a95d1f3ee4091665cac1cd4a6a8f47ef3103b
Mike Cramer <imanerd@me.com>
2019-02-25 10:51:58 -0600

    That's embarassing

--- dev/git_summary.sh
+++ dev/git_summary.sh
@@ -67,7 +67,7 @@ for inputDir in $input; do
                 echo
             fi
             cd "$launchDir"
-        done <<< `find -L "${inputDir}" -name '.git'`
+        done <<< `find  -L "${inputDir}" -name '.git' -type d`
     fi
 done
 
