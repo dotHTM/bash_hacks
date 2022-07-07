c04a171830ec1cbc3fe7f904fa6ede89feb62a49
Mike Cramer <dothtm@icloud.com>
2022-07-07 17:53:39 -0500

    scheduled optional insomnia

--- util/insomnia.sh
+++ util/insomnia.sh
@@ -2,16 +2,46 @@
 # insomnia.sh
 
 
+echo "################################"
+date
+
 
 while [[ 1 == 1 ]]; do
+
+    # echo  "lunch time"
+
     now=`date "+%s"`
-    saturday=`date -v6w -v17H -v30M "+%s"`
-    declare -i delta=${saturday}-${now} 
-    # echo $saturday
-    # echo $now
-    echo $delta
-    if [[ $delta -gt 0 ]]; then
-        caffeinate -disu -t $delta
+    nextSleep=`date -v11H -v30M "+%s"`
+    declare -i delta=${nextSleep}-${now}-60
+    if [[ -z ${delta/0-9//} ]]; then
+        # echo "delta     $delta"
+        # echo "now       $now"
+        # echo "nextSleep $nextSleep"
+        date -v+${delta}S
+        echo $delta
+        if [[ $delta -gt 0 ]]; then
+            caffeinate -disu -t $delta
+        fi
+        sleep 3600
     fi
-    sleep 1
+    
+    # echo  "end of the work day"
+    
+    now=`date "+%s"`
+    
+    nextSleep=`date -v16H -v30M "+%s"`
+    declare -i delta=${nextSleep}-${now}-60
+    if [[ -z ${delta/0-9//} ]]; then
+        # echo "delta     $delta"
+        # echo "now       $now"
+        # echo "nextSleep $nextSleep"
+        date -v+${delta}S
+        echo $delta
+        if [[ $delta -gt 0 ]]; then
+            caffeinate -disu -t $delta
+        fi
+        # sleep 7200
+    fi
+    
+    sleep 10
 done
