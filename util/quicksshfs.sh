c88e2e3dffd5332a2d39d03cc289d54f8e82d2cf
Mike Cramer <dothtm.comments@gmail.com>
2023-08-03 11:29:19 -0500

    Eject all option.

--- util/quicksshfs.sh
+++ util/quicksshfs.sh
@@ -4,6 +4,9 @@
 
 defaultMountParent="/tmp/vol/"
 
+platform=`uname`
+
+
 usage() {
   if [[ $DEBUG_MODE ]]; then
     echo "
@@ -13,7 +16,10 @@ usage() {
     "
   fi
 
-  echo "Usage: ${0/*\/} [-umosfl] -r [user@]server.com:/remote/path [-v volumeName [-p /path/to/mountPoint]] "
+  echo "Usage: 
+  ${0/*\/} [-ufmonlsebPVE] -r [user@]server.com:/remote/path [-v volumeName [-p /path/to/mountPoint]]
+  ${0/*\/} [-uf]
+  "
   echo
   echo "  If [/path/to/mountPoint] is ommitted, then /tmp will be the parent folder to the mounted file system."
   echo
@@ -35,29 +41,83 @@ usage() {
   exit 1
 }
 
+ifEcho(){
+  level=$1 && shift
+  message=$1 && shift
+  if (( ${level} )); then
+    echo $message
+  fi
+}
+
+
+unmount(){
+  path=$1 && shift
+  verbose=$1 && shift
+  ifEcho $verbose "unmounting      : ${path}"
+  if [[ -d "${path}" ]]; then
+    if [[ "$platform" == "Darwin" ]]; then
+      diskutil unmount "${path}" 1>/dev/null
+    else
+      echo "idk what to do for your platform to unmount this mount point..." >&2
+    fi
+    trashMountPoint "${path}"
+  fi
+}
+
+forceUnmount(){
+  path=$1 && shift
+  verbose=$1 && shift
+  ifEcho $verbose "force unmounting: ${path}"
+  if [[ -d "${path}" ]]; then
+    if [[ "$platform" == "Darwin" ]]; then
+      diskutil unmount force "${path}" 1>/dev/null
+    else
+      echo "idk what to do for your platform to unmount this mount point..." >&2
+    fi
+    trashMountPoint "${path}"
+  fi
+}
+
+
+trashMountPoint(){
+  mp=$1 && shift
+  if [[ -d $mp ]]; then
+    if [[ "$platform" == "Darwin" ]]; then
+      mv "$mp" ~/.Trash/"${mp/*\//}__`date "+%s"`"
+    else
+      echo "idk what to do for your platform to remove/trash this mount point..." >&2
+    fi
+  fi
+}
+
+
+
+
+
+
 while getopts "hr:v:p:umosbnedflPVE" inputOptions; do
   case "${inputOptions}" in
-    h) usage ;;                ##
+  h) usage ;;                ##
     ##
-    r) connection=${OPTARG} ;; ##
-    v) volumeName=${OPTARG} ;; ##
-    p) mountPoint=${OPTARG} ;; ##
+  r) connection=${OPTARG} ;; ##
+  v) volumeName=${OPTARG} ;; ##
+  p) mountPoint=${OPTARG} ;; ##
     ##
-    u) unmountMode=1 ;;        ##
-    f) forceUnmountMode=1 ;;   ##
-    m) mount_mode=1 ;;         ##
-    o) openInSublime=1 ;;      ##
-    n) openInSublimeNewWindow=1 ;;      ##
-    l) openInLocalShell=1 ;;      ##
-    s) shellOpen=1 ;;          ##
-    e) screenOpen=1 ;;          ##
-    b) moshOpen=1 ;;           ##
-    d) DEBUG_MODE=1;;          ##
-    P) print_mountpoint=1;;          ##
-    V) print_volume=1;;          ##
-    E) echoOut=1;;          ##
+  u) unmountMode=1 ;;        ##
+  f) forceUnmountMode=1 ;;   ##
+  m) mount_mode=1 ;;         ##
+  o) openInSublime=1 ;;      ##
+  n) openInSublimeNewWindow=1 ;;      ##
+  l) openInLocalShell=1 ;;      ##
+  s) shellOpen=1 ;;          ##
+  e) screenOpen=1 ;;          ##
+  b) moshOpen=1 ;;           ##
+  d) DEBUG_MODE=1;;          ##
+  P) print_mountpoint=1;;          ##
+  V) print_volume=1;;          ##
+  E) echoOut=1;;          ##
     ##
-    *) usage ;;                ##
+  *) usage ;;                ##
     ##
   esac
 done
@@ -72,11 +132,27 @@ remoteSansColon="${connection/:/}"
 remoteHost="${connection/:*/}"
 remotePath="${connection/*:/}"
 
-if [[ $connection == $remoteSansColon ]]; then echo "Incorrect remote format"; usage; fi ##
-if [[ -z "${connection}" ]]; then echo "No connection specified"; usage; fi              ##
-if [[ -z "${remoteHost}" ]]; then echo "No remote Host specified"; usage; fi         ##
-if [[ -z "${remotePath}" ]]; then echo "No remote Path specified"; usage; fi         ##
-##
+if [[ -z "${connection}" ]];then
+  ifEcho $echoOut "No connection, eject all mode."
+  if (( ${unmountMode}  )); then
+    for path in `find ${defaultMountParent} -type d -d 1`; do
+      unmount "${path}" $echoOut
+    done
+    exit
+  fi
+  if (( ${forceUnmountMode}  )); then
+    for path in `find ${defaultMountParent} -type d -d 1`; do
+      forceUnmount "${path}" $echoOut
+    done
+    exit
+  fi
+fi
+
+if [[ -z "${connection}" ]]; then echo "No connection specified"; usage; fi #             
+if [[ -z "${remoteHost}" ]]; then echo "No remote Host specified"; usage; fi #        
+if [[ -z "${remotePath}" ]]; then echo "No remote Path specified"; usage; fi #        
+if [[ $connection == $remoteSansColon ]]; then echo "Incorrect remote format"; usage; fi #
+
 
 if [[ -z "$volumeName" ]]; then
   volumeName=`echo "$connection" | cut -d "@" -f 2 | tr ":/." "_--"`
@@ -89,41 +165,12 @@ if [[ -z "$connection" || -z "$mountPoint" || -z "$volumeName" ]]; then
 fi
 
 
-platform=`uname`
 
-
-## Action modes
-
-trashMountPoint(){
-  mp=$1 && shift
-  if [[ -d $mp ]]; then
-    if [[ "$platform" == "Darwin" ]]; then
-      mv "$mp" ~/.Trash/"${mp/*\//}__`date "+%s"`"
-    else
-      echo "idk what to do for your platform to remove/trash this mount point..." >&2
-    fi
-  fi
-}
-
-
+if (( $unmountMode )); then
+  unmount "${mountPoint}" $echoOut
+fi
 if (( $forceUnmountMode )); then 
-  if [[ -d "${mountPoint}" ]]; then
-    if [[ "$platform" == "Darwin" ]]; then
-      diskutil unmount force "${mountPoint}" 1>/dev/null
-    else
-      echo "idk what to do for your platform to unmount this mount point..." >&2
-    fi
-    trashMountPoint "${mountPoint}"
-  fi
-elif (( $unmountMode )); then
-  if [[ -d "${mountPoint}" ]]; then
-    if [[ "$platform" == "Darwin" ]]; then
-      diskutil unmount "${mountPoint}" 1>/dev/null
-    else
-      echo "idk what to do for your platform to unmount this mount point..." >&2
-    fi
-    trashMountPoint "${mountPoint}"
-  fi
+  forceUnmount "${mountPoint}" $echoOut
 fi
 
 if (( "$mount_mode" )); then
@@ -143,36 +190,35 @@ elif (( "$openInSublime" )); then
   subl -a "$mountPoint"
 fi
 
-if [[ -n "$connection" ]]; then
-  
-  domain="${connection/:*}"
-  path="${connection/*:}"
-  
-  
+
+
+domain="${connection/:*}"
+path="${connection/*:}"
+
+if [[ -n "$echoOut" ]]; then
+  echo "$domain => \"$path\""
+fi
+
+if [[ -n "$openInLocalShell" ]]; then
   if [[ -n "$echoOut" ]]; then
-    echo "$domain => \"$path\""
+    echo "$mountPoint"
   fi
-  
-  if [[ -n "$openInLocalShell" ]]; then
-    if [[ -n "$echoOut" ]]; then
-      echo "$mountPoint"
-    fi
-  else
-    if [[ "$shellOpen" || "$moshOpen" ]]; then
-      if (( "$moshOpen" )); then
-        if [[ -n "$echoOut" ]]; then
-          echo "; mosh "$domain" -- bash -c \"cd \\\"$path\\\"; ${shellInvokationCommand}\""
-        fi
-        mosh "$domain" -- bash -c "cd \"$path\"; ${shellInvokationCommand}"
-      else
-        if [[ -n "$echoOut" ]]; then
-          echo "; ssh "$domain" -t \"cd \\\"$path\\\"; ${shellInvokationCommand};\""
-        fi
-        ssh "$domain" -t "cd \"$path\"; ${shellInvokationCommand};"
+else
+  if [[ "$shellOpen" || "$moshOpen" ]]; then
+    if (( "$moshOpen" )); then
+      if [[ -n "$echoOut" ]]; then
+        echo "; mosh "$domain" -- bash -c \"cd \\\"$path\\\"; ${shellInvokationCommand}\""
       fi
+      mosh "$domain" -- bash -c "cd \"$path\"; ${shellInvokationCommand}"
+    else
+      if [[ -n "$echoOut" ]]; then
+        echo "; ssh "$domain" -t \"cd \\\"$path\\\"; ${shellInvokationCommand};\""
+      fi
+      ssh "$domain" -t "cd \"$path\"; ${shellInvokationCommand};"
     fi
   fi
 fi
+
 
 if [[ -n $print_mountpoint ]]; then
   echo "$mountPoint"
