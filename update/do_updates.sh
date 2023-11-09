9cb5e4033cc50c0377162667a39e8dde91c104ce
Mike Cramer <dothtm.comments@gmail.com>
2023-11-09 15:57:13 -0600

    snapshot

--- update/do_updates.sh
+++ update/do_updates.sh
@@ -4,14 +4,21 @@
 echo "==== Homebrew ================================"
 brew update
 brew upgrade
+brew upgrade --cask --greedy
 brew cleanup
 echo
+
 echo "==== Python/pip ================================"
 pip install --upgrade pip
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 echo
+
 echo "==== Ruby/gem ================================"
 gem update --system
 gem update
 gem cleanup
+echo
+
+echo "==== macOS ================================"
+softwareupdate --install --recommended
 echo
--- util/insomnia.sh
+++ util/insomnia.sh
@@ -2,46 +2,11 @@
 # insomnia.sh
 
 
-echo "################################"
-date
-echo "----------------------------"
+read min sec <<<$(date +'%M %S')
+seconds_until_the_next_hour=$(( 3600 - 10#$min*60 - 10#$sec ))
+wait_time=$(( $seconds_until_the_next_hour - 10 )) # nudge it a little short.
 
+# echo $seconds_until_the_next_hour
+# echo $wait_time
 
-while [[ 1 == 1 ]]; do
-
-
-    now=`date "+%s"`
-    nextSleep=`date -v11H -v30M "+%s"`
-    declare -i delta=${nextSleep}-${now}-60
-    if [[ "$delta"  =~ ^[0-9]+$ ]]; then
-        echo  "waiting for lunch time"
-        # echo "delta     $delta"
-        # echo "now       $now"
-        # echo "nextSleep $nextSleep"
-        date -v+${delta}S
-        echo $delta
-        if [[ $delta -gt 0 ]]; then
-            caffeinate -disu -t $delta
-        fi
-        sleep 3600
-    fi
-    
-    
-    now=`date "+%s"`\
-    nextSleep=`date -v16H -v30M "+%s"`
-    declare -i delta=${nextSleep}-${now}-60
-    if [[ "$delta"  =~ ^[0-9]+$ ]]; then
-        echo  "waiting for the end of the work day"
-        # echo "delta     $delta"
-        # echo "now       $now"
-        # echo "nextSleep $nextSleep"
-        date -v+${delta}S
-        echo $delta
-        if [[ $delta -gt 0 ]]; then
-            caffeinate -disu -t $delta
-        fi
-        # sleep 7200
-    fi
-    
-    sleep 60
-done
+/usr/bin/caffeinate -disu -t $wait_time
