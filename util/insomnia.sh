768d145d352b380be7a5ddfa7fa8cae7527abc5d
Mike Cramer <mikey@utexas.edu>
2024-01-23 14:16:49 -0600

    insomnia on a schedule, 8-5 and lunch at noon-1

--- util/insomnia.sh
+++ util/insomnia.sh
@@ -2,11 +2,44 @@
 # insomnia.sh
 
 
-read min sec <<<$(date +'%M %S')
-seconds_until_the_next_hour=$(( 3600 - 10#$min*60 - 10#$sec ))
-wait_time=$(( $seconds_until_the_next_hour - 10 )) # nudge it a little short.
+time_to(){
+    message=$1 && shift
+    hour=$1 && shift
+    minute=$1 && shift
+    next_day=$1 && shift
+    
+    if [[ -z $minute ]]; then
+        until="${message} ${hour}"
+        minute="00"
+    else
+        until="${message} ${hour}:${minute}"
+    fi
+    
+    mkdir -p ~/.config/variables
+    echo "${until}" > ~/.config/variables/insomnia.txt
+    
+    if [[ -z $next_day ]]; then
+        echo $(( `date -v${hour}H -v${minute}M -v0S "+%s"` - `date "+%s"` ))
+    else
+        echo $(( `date -v${hour}H -v${minute}M -v0S -v+${next_day}d "+%s"` - `date "+%s"` ))
+    fi
+}
 
-# echo $seconds_until_the_next_hour
-# echo $wait_time
+wait_until(){
+    hour=$1 && shift
+    minute=$1 && shift
+    next_day=$1 && shift
+    sleep $( time_to "" $hour $minute $next_day )
+}
+buzz_until(){
+    hour=$1 && shift
+    minute=$1 && shift
+    next_day=$1 && shift
+    caffeinate -disu -t $( time_to "☕️" $hour $minute $next_day )
+}
 
-/usr/bin/caffeinate -disu -t $wait_time
+wait_until 8
+buzz_until 12
+wait_until 13
+buzz_until 17
+wait_until "00" "00" 1
