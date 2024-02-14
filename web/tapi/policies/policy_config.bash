cc6016fcdb8041715a717bf2b323449d63e55886
Mike Cramer <dotHTM@users.noreply.github.com>
2024-02-14 10:32:12 -0600

    Delete web directory

--- web/tapi/policies/policy_config.bash
+++
@@ -1,9 +0,0 @@
-####
-
-export MAIN_ACCOUNT="dothtm"
-export SECONDARY_ACCOUNT="kiodane"
-
-export MAIN_TO_SECONDARY_TRANSFER_LIST="private-list-to-transfer"
-export SECONDARY_TO_MAIN_TRANSFER_LIST="private-list-to-transfer"
-
-
