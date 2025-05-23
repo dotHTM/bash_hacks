6c279990fc1a1e84be00d477eb5c68f3ff3eb21b
Mike Cramer <dotmike@utexas.edu>
2025-05-23 16:42:25 -0500

    snapshot

--- docker/dkFullReset.sh
+++ docker/dkFullReset.sh
@@ -4,16 +4,19 @@
 if [[ -n `which docker` ]]; then
     echo "== Containers =="
     for i in $(docker ps --all -q); do
-        docker rm -f $i
+        docker rm --force $i
     done
     echo "== Images =="
     for i in $(docker images --all -q); do
-        docker rmi -f $i
+        docker rmi --force $i
     done
     echo "== Volumes =="
     for i in $(docker volume ls -q); do
         docker volume rm $i
     done
+    echo "== Builder =="
+    docker builder prune --all --force
+
 else
     echo "Docker not installed"
 fi
--- update/do_updates.sh
+++ update/do_updates.sh
@@ -1,24 +1,63 @@
 #!/usr/bin/env bash
 # do_updates.sh
 
-echo "==== Homebrew ================================"
-brew update
-brew upgrade
-brew upgrade --cask --greedy
-brew cleanup
-echo
+
+set -e 
+
+ms_au_app="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app"
+if [[ -e "$ms_au_app" ]]; then
+    echo "==== MS Office ================================"
+    osascript -e 'tell app "outlook" to quit'
+    osascript -e 'tell app "microsoft teams" to quit'
+    open "$ms_au_app"
+    echo
+fi
+
+if [[ "Darwin" == $(uname) ]]; then
+    echo "==== macOS ================================"
+    softwareupdate --install --recommended
+    echo
+fi
+
+if [[ -n $(which brew) ]]; then
+    echo "==== Homebrew ================================"
+    brew update
+    brew upgrade
+    brew upgrade --cask --greedy
+    brew cleanup
+    echo
+fi
 
 echo "==== Python/pip ================================"
-pip install --upgrade pip
-pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+pips="pip3.7 pip3.8 pip3.9 pip3.10 pip3.11"
+echo "### Pips:"
+for this_pip in $pips; do
+    if [[ -n $(which $this_pip) ]]; then
+        echo "- $this_pip"
+    fi
+done
+for this_pip in $pips; do
+    if [[ -n $(which $this_pip) ]]; then
+        echo "==== $this_pip ===="
+        # $this_pip list
+        $this_pip install --upgrade pip
+        $this_pip freeze --local \
+            | grep -v '^\-e' \
+            | cut -d = -f 1  \
+            | xargs -n1 $this_pip install -U
+    fi
+done
 echo
+if [[ -n $(which pipx) ]]; then
+echo "==== pipx ================================"
+pipx upgrade (pipx list --json | jq -r ".venvs | keys[]")
+echo
+fi
 
+if [[ -n $(which gem) ]]; then
 echo "==== Ruby/gem ================================"
 gem update --system
 gem update
 gem cleanup
 echo
-
-echo "==== macOS ================================"
-softwareupdate --install --recommended
-echo
+fi
