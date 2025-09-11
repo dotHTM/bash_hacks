d86eced12d4f927c403fddaa04c075bd8fafda5a
Mike Cramer <dotmike@utexas.edu>
2025-09-11 09:22:37 -0500

    test

---
+++ util/test_line_endings.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# test_line_endings.sh
+
+
+
+echo "test line endings"
+echo "1 Endings are importand in computer science.
+2 Endings are importand in computer science. Endings are importand in computer science.
+3 Endings are importand in computer science. Endings are importand in computer science. Endings are importand in computer science.
+4 Endings are importand in computer science. Endings are importand in computer science. Endings are importand in computer science. Endings are importand in computer science."
+echo
+echo "nl Endings are importand in computer science."
